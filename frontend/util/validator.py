4b009bbd619442c156dc262159928a9d2f6b2374
brinza <bezrukoff888@gmail.com>
2022-01-20 15:33:37 +0300

    Added validator to check version Added error message about incorrect version string

--- frontend/util/validator.py
+++ frontend/util/validator.py
@@ -1,3 +1,9 @@
+import re
+
+
+version_pattern = re.compile(r"^((?:[1-9][0-9]*?)|0)(?:\.((?:[1-9][0-9]*?)|0))?(?:\.((?:[1-9][0-9]*?)|0))?$")
+
+
 def check_if_empty(val, par_name="Value"):
     if not val.strip():
         raise ValueError("%s should not be empty string" % par_name)
@@ -7,4 +13,13 @@ def check_if_empty(val, par_name="Value"):
 def check_if_number(val, par_name="Value"):
     if not val.isdigit() and val.strip():
         raise ValueError("%s should be a number" % par_name)
+    return val
+
+
+def check_if_version(val, par_name="Version"):
+    match = version_pattern.match(val)
+    if not match:
+        raise ValueError("{} should match pattern x.y.z, x.y or just x (e.g. '1.1.1', '1.1' or just '1')".format(
+            par_name
+        ))
     return val
