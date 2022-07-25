1da0575e45fc67f4e78d130f200e21c21c4018d4
mikheev <mikheev@physlab.ru>
2022-07-25 10:14:20 +0300

    Notice msg fied

--- urpc/builder/library/clib.py
+++ urpc/builder/library/clib.py
@@ -1255,7 +1255,7 @@ class _ClibBuilderImpl(ClangView):
             )
         ELSE()
             MESSAGE(
-                NOTICE "Won't hide non-API library symbols. Оn some platforms it may cause segmentation faults."
+                NOTICE " Won't hide non-API library symbols. Оn some platforms it may cause segmentation faults."
             )
         ENDIF()
         IF(${{CMAKE_SYSTEM_NAME}} STREQUAL Darwin)
